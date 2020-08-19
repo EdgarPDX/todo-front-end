36ee513b214e2677b1eec5930ce6a4e40d177cde
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-08-19 16:48:53 -0700

    added AuthPage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+  } 
+from 'react-router-dom';
+import AuthPage from './AuthPage.js';
+import DetailTodo from './DetailTodo.js';
+import ListTodos from './ListTodos.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/login" 
+                            exact
+                            render={(routerProps) => <AuthPage {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <ListTodos {...routerProps} />} 
+                        />
+                        <Route 
+                          path="/todos/:id" 
+                          exact
+                          render={(routerProps) => <DetailTodo {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/ListTodos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListTodos extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
